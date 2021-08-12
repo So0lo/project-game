6a01abe288b2efa3b946b92cd1bb501e0c583589
Web <web@it.step>
2021-08-12 21:17:32 +0300

    new fold

---
+++ scripts/utils/generateTreasureCoords.js
@@ -0,0 +1,8 @@
+import {generateRandomNumber} from './generateRandomNumber.js';
+
+const generateTreasureCoords = ({width, height}, gap) => ({
+    x: generateRandomNumber(width, gap),
+    y: generateRandomNumber(height, gap)
+});
+
+export {generateTreasureCoords};
