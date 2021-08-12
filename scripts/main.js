c9f81943598ecad9311fd038048282fe0b88e0a8
Web <web@it.step>
2021-08-12 19:57:24 +0300

    implemented func

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
 const hint = document.getElementById(`hint`);
 const map = document.querySelector(`[alt="map"]`);
 
+function getDistanceToTreasure ({x : cX, y: cY}, 
+        {x : tX, y: tY}) {
+        return Math.sqrt((cX - tX)**2 + (cY - tY)**2);
+}
