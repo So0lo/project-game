85a209a49a6f0d6819b5cf2ba60ae0a77c856c64
Web <web@it.step>
2021-08-12 20:10:46 +0300

    n

--- scripts/main.js
+++ scripts/main.js
@@ -8,5 +8,3 @@ const generateTreasureCoords = ({width, height}, gap) => ({
         x: generateRandomNumber(width, gap),
         y: generateRandomNumber(height, gap)
 });
-
-console.log(generateTreasureCoords(treasureMap, 50));
